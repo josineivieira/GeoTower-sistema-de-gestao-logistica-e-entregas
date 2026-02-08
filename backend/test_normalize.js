df386abb4aac9ca4aacdf651ea727b64c52e5253
Your Name <Josinei@GEOLOGNB24.geomaritima.local>
2026-02-08 11:04:03 -0400

    cleanup: remove test file

--- backend/test_normalize.js
+++
@@ -1,9 +0,0 @@
-const { normalizeDeliveryForResponse } = require('./src/utils/storageUtils');
-
-const sample = {
-  documents: {
-    canhotNF: ['[{"name":"NF_1770561417949_0.jpg"}]','{"path":"manaus/BBB/NF_1770561417949_0.jpg"}']
-  }
-};
-
-console.log(JSON.stringify(normalizeDeliveryForResponse(sample), null, 2));
